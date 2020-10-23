d403decdcc67a4afcbafcab99ea3cdd61c97a5cf
rkeeves <aprettydecentaddress@gmail.com>
2020-10-23 22:00:44 +0200

    Changed Start class
    Start is still the entry point, but now it instantiates an instance
    of Application and delegates actual start-up and gameloop to it.
    
    I didn't want to override/rename Start class, because it is an
    entry point, and it would throw off Intellij run configs for everyone.

--- src/main/java/hu/unideb/inf/Start.java
+++ src/main/java/hu/unideb/inf/Start.java
@@ -1,12 +1,9 @@
 package hu.unideb.inf;
 
-import hu.unideb.inf.room.FirstRoom;
-
 public class Start {
 
     public static void main(String[] args) {
-        FirstRoom firstRoom = new FirstRoom();
-        firstRoom.enter();
+        new Application().run(args);
     }
 
 }
