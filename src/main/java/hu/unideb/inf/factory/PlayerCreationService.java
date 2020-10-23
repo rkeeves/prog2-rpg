eac58878620e4e3708a9714f1d1285dcab294f7c
rkeeves <aprettydecentaddress@gmail.com>
2020-10-23 21:54:05 +0200

    Changed PlayerCreationService
    This change was just a quick and dirty hack to add a method for storing
    the player name String before the actual call to create.

--- src/main/java/hu/unideb/inf/factory/PlayerCreationService.java
+++ src/main/java/hu/unideb/inf/factory/PlayerCreationService.java
@@ -4,14 +4,16 @@ import hu.unideb.inf.io.ConsoleIOService;
 import hu.unideb.inf.io.IOService;
 import hu.unideb.inf.model.Player;
 
-public class PlayerCreationService implements EntityFactory {
+public class PlayerCreationService implements EntityFactory{
+
+    private String playerName = "";
 
     @Override
     public Player create() {
         IOService ioService = new ConsoleIOService();
 
         return Player.builder()
-                .name(ioService.getPlayerName())
+                .name(playerName)
                 .gold(10)
                 .damage(5)
                 .health(100)
@@ -20,4 +22,7 @@ public class PlayerCreationService implements EntityFactory {
                 .build();
     }
 
+    public void setPlayerName(String name){
+        this.playerName = name;
+    }
 }
